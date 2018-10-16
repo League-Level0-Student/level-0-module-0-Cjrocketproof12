77c5f871b2235e278d48942394cfc18a3335e80b
Cjrocketproof12 <league@WTS-IM-6.attlocal.net>
2018-10-16 16:58:30 -0700

    L0 Project (The First)

---
+++ src/strings_and_dialogs/RobotG.java
@@ -0,0 +1,39 @@
+package strings_and_dialogs;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotG {
+public static void main(String[] args) {
+	Robot Frank=new Robot();
+			Frank.penDown();
+			Frank.setSpeed(1000);
+			Frank.move(100);
+			Frank.turn(90);
+			Frank.move(100);
+			Frank.turn(-180);
+			Frank.move(100);
+			Frank.turn(360);
+			Frank.turn(-90);
+			Frank.move(200);
+			Frank.turn(-90);
+			Frank.move(100);
+			Frank.turn(180);
+			Frank.move(100);
+			Frank.turn(90);
+			Frank.move(100);
+			Frank.turn(90);
+			Frank.penUp();
+			Frank.move(50000000);
+			
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
--- src/strings_and_dialogs/RobotSquare.java
+++ src/strings_and_dialogs/RobotSquare.java
@@ -12,22 +12,25 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot C3PO=new Robot();
 
         // 3. Put the robot's pen down
-
+C3PO.penDown();
 
         // 6. Make the robot move as fast as possible
 
-
+C3PO.setSpeed(1);
         // 5. Do everything below here 4 times
-
+for(int i=0;i<4;i++)
+{
 
         //         2. Move your robot 200 pixels
-
+C3PO.move(300);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
-
+C3PO.turn(90);
+}
     }
 }
+
+
